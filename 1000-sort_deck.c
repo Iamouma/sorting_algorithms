9c9f7ee9c983bcb549b7cf14fe66309d12c829e0
Iamouma <stephenouma452@gmail.com>
2024-01-23 11:10:34 -0800

    A  function that sorts a deck of cards.

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -1,9 +1,4 @@
 #include "deck.h"
-int _strcmp(const char *s1, const char *s2);
-char get_value(deck_node_t *card);
-void insertion_sort_deck_kind(deck_node_t **deck);
-void insertion_sort_deck_value(deck_node_t **deck);
-void sort_deck(deck_node_t **deck);
 
 /**
  * _strcmp - compares two strings.
